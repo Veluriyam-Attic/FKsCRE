f2e61d3686a87b5b19f1e9f76adf440ffe1de537
Nan_Ting <52440522+3380991070@users.noreply.github.com>
2024-09-16 15:05:30 +0800

    添加飓风箭

---
+++ Content/Ammunition/HurricaneArrow/HurricaneArrow.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NanTing.Content.Ammunition.JuZhiShi
+{
+    /// <summary>
+    /// 飓风箭 -> 天蓝箭
+    /// </summary>
+    public class HurricaneArrow : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 7;
+            //箭矢
+            Item.ammo = AmmoID.Arrow;
+            Item.shoot = ModContent.ProjectileType<HurricaneArrow_Proje>();
+            base.SetDefaults();
+        }
+    }
+
+    public class HurricaneArrow_Proje : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.damage = 5;
+            Projectile.friendly = true;
+            Projectile.aiStyle = 1;
+            base.SetDefaults();
+        }
+
+        public override void AI()
+        {
+            Vector2 MouseVectorWorld = Main.MouseWorld;
+            Vector2 PlayerVectorWorld = Main.player[Projectile.owner].Center;
+
+            base.AI();
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            //Projectile.NewProjectile(default,)
+            base.OnKill(timeLeft);
+        }
+    }
+
+}
