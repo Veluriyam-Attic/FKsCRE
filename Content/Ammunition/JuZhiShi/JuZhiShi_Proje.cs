c0d3d314f7e796981a06fc1617b1f6113c5ec4b0
23759 <23759@LAPTOP-JASD97B2>
2024-09-16 14:41:07 +0800

    添加项目文件。

---
+++ Content/Ammunition/JuZhiShi/JuZhiShi_Proje.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace NanTing.Content.Ammunition.JuZhiShi
+{
+    public class JuZhiShi_Proje : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.damage = 5;
+            Projectile.friendly = true;
+            Projectile.aiStyle = 1;
+            base.SetDefaults();
+        }
+
+        public override void AI()
+        {
+            Vector2 MouseVectorWorld = Main.MouseWorld;
+            Vector2 PlayerVectorWorld = Main.player[Projectile.owner].Center;
+
+            base.AI();
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            //Projectile.NewProjectile(default,)
+            base.OnKill(timeLeft);
+        }
+    }
+}
